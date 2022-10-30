e4ac65eab2f44dc3534e4f5383121414536d3c9a
kaito <103406976+kaitokuroda@users.noreply.github.com>
2022-10-30 23:27:08 +0900

    プロジェクト作成

---
+++ prmn2022a/src/lecture01/Kadai1.java
@@ -0,0 +1,7 @@
+package lecture01;
+
+public class Kadai1 {
+    public static void main(String[] args) {
+        System.out.println("xxxxxxxxx");
+    }
+}
---
+++ prmn2022a/src/lecture01/Kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Kadai2 {
+    public static void main(String[] args) {
+        int studentNumber = 1111111;
+        System.out.println("B" + studentNumber + " xxxxxxxxx ");
+    }
+}

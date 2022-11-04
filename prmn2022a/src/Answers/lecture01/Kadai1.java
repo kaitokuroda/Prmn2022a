44a0435058ae92de5080e1637f4a6c0c42e5d426
kaito <103406976+kaitokuroda@users.noreply.github.com>
2022-11-04 19:44:24 +0900

    first commit

---
+++ prmn2022a/src/Answers/lecture01/Kadai1.java
@@ -0,0 +1,2 @@
+package Answers.lecture01;public class Kadai1 {
+}
--- prmn2022a/src/lecture01/Kadai1.java
+++
@@ -1,7 +0,0 @@
-package lecture01;
-
-public class Kadai1 {
-    public static void main(String[] args) {
-        System.out.println("xxxxxxxxx");
-    }
-}
